350c2e317164240ff09b86b17a5a4f3cc67a2d13
Zombox <ghostzb@outlook.com>
2023-03-25 22:09:12 +0700

    edit9

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import TestPage from "pages/TestPage";
 import ErrorPage from "pages/ErrorPage";
 
 //Components
-import Navbar from "./components/navbar/NavbarX";
+import Navbar from "components/navbar/NavbarZ";
 import PrivateRoute from "components/PrivateRoute";
 
 //toast
