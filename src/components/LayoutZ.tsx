9c65cab4219879549f375070b0c9c14e0b84c552
Zombox <ghostzb@outlook.com>
2023-03-26 01:33:48 +0700

    edit navigatebar

--- src/components/LayoutZ.tsx
+++ src/components/LayoutZ.tsx
@@ -7,7 +7,7 @@ import useAuthenticationStore from "store/authentication/authentication.store";
 //components
 // import OnCreateLi from "./navbar/OnCreateLi";
 // import HamburgerMenu from "./navbar/HamburgerMenu";
-import DropdownProfile from "./navbar/DropdownProfile";
+import DropdownProfile from "./navigatebar/DropdownProfile";
 import Footer from "components/Footer";
 
 //reference https://www.ramotion.com/web-design/?utm_source=drbl&utm_medium=special&utm_campaign=20247474-Education-Website
